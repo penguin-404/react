d0e7450de407d79207aa5941210cf43ee3540ba9
Kaushal <077bct020.kaushal@sagarmatha.edu.np>
2024-10-23 21:42:22 +0545

    evaluated expression

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    
+    <h1>Hello Duniya</h1>
+    
+  )
+}
+
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://www.google.com/',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <h1>Hello there</h1>
+)
+
+const username = "kaushal";
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://www.google.com/',target:'_blank'},'Click me to visit google',username
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
+
 )
